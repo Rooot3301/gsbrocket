63eeb907d852b1e26e1013a61b7108b3c9c573eb
romain <romain@ro-kali>
2024-02-16 16:47:08 +0100

    Modification script AD

---
+++ Script/Windows/importuserADLAURENT/README.md
@@ -0,0 +1,24 @@
+# Script-Users-AD
+
+Simple petit script Powershell permettant de créer des utilisateurs dans un Active Directory en se basant sur un classeur au format CSV.
+
+✅ Testé sous Windows Server 2012, 2012 R2, et 2016
+
+➡ Télécharger le script
+
+➡ Télécharger l'exemple de fichier CSV
+
+➡ Éditer le fichier CSV en rajoutant les différents utilisateurs
+
+➡ Placer le fichier CSV dans un dossier nommé "C:\Windows\Scripts\"
+
+➡ Éditer la partie du script relative au nom de domaine (-UserPrincipalName "$Username@notamax.local")
+
+➡ Exécuter le script, et prendre une tasse de café avec le temps économisé
+
+
+
+
+
+
+⚠ Étant donné la simplicité de ce script, aucun support n'est disponible pour celui-ci. Si vous rencontrez une erreur, relisez bien la méthode d'installation ou bien effectuez une recherche sur Internet.
---
+++ Script/Windows/importuserADLAURENT/script_ad_users.ps1
@@ -0,0 +1,36 @@
+###############################################
+#            Varene Romain, 2024            #
+#     Script de création Users AD via Calc    #
+# Librement repris de celui de Thomas Limpens #
+###############################################
+
+# On importe le module Active Directory
+Import-Module ActiveDirectory
+
+# On utilise le module Import-csv sur notre calc pour en faire une variable
+$CalcAD = Import-csv -Delimiter ";" -Path C:\Scripts\Calc_powershell_script.csv
+
+# Boucle foreach 
+foreach ($User in $CalcAD)
+{
+       $Username    = $User.Username
+       $Password    = $User.password
+       $Prenom      = $User.Prenom
+       $Nom         = $User.Nom
+       $Chemin      = $User.Chemin         
+
+
+       # On vérifie si l'utilisateur n'existe pas déjà dans le domaine
+       if (Get-ADUser -F {SamAccountName -eq $Username}) {
+
+           # On affiche un message sous forme de Warning
+           Write-Warning "Oops, on dirait que $Username est déjà présent dans l'AD..."
+       }
+       else {
+
+              # Sinon on créer l'utilisateur. Bien prendre soin de modifier le nom de domaine 
+              # ici avant d'exécuter le script !
+              New-ADUser -SamAccountName $Username -UserPrincipalName "$Username@notamax.local" -Name "$Prenom $Nom" -GivenName $Prenom -Surname $Nom -Enabled $True -DisplayName "$Nom, $Prenom" -Path $Chemin -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+       }
+}
